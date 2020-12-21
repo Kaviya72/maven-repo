945dab4c4f9e3f5e246cc610c973f7e646dd6c68
kaviya72 <kaviyasrini1999@gmail.com>
2020-12-21 14:59:19 +0530

    added sub method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -4,7 +4,11 @@ public class Calc {
 	public static int add(int num1,int num2) {
 		return num1+num2;
 	}
+	public static int subs(int num1,int num2) {
+		return num1-num2;
+	}
 	public static void main(String args[]) {
-		System.out.println(add(10,20));
+		System.out.println("sum: "+ add(10,20));
+		System.out.println("difference: " + subs(90,20));
 	}
 }
