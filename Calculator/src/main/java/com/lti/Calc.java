b4d8e84c9856fa9a90dff74878ba8920a6292079
kaviya72 <kaviyasrini1999@gmail.com>
2020-12-21 15:10:08 +0530

    added prod method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -7,8 +7,12 @@ public class Calc {
 	public static int subs(int num1,int num2) {
 		return num1-num2;
 	}
+	public static int prod(int num1,int num2) {
+		return num1*num2;
+	}
 	public static void main(String args[]) {
 		System.out.println("sum: "+ add(10,20));
 		System.out.println("difference: " + subs(90,20));
+		System.out.println("prod: "+ prod(20,10));
 	}
 }
